4a539103abfe5aa901ae8359859dc44bbd829903
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-02-25 01:12:55 -0300

    fix: Change form class name

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react"
 import Select from "react-select"
 
-class AddBookForm extends Component {
+class BookForm extends Component {
   constructor(props) {
     super(props)
 
