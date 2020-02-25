42fc268e50bed40ab48fda168d4490edfbb79726
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-02-25 00:46:46 -0300

    feat: Show all books in local storage

--- src/App.js
+++ src/App.js
@@ -15,9 +15,38 @@ class App extends Component {
     
   return (
     <div className="App">
-      <Book title="fantasy world" author="me" tag="read" rating="1 star"/>
+      {
+        ShowBooks()
+      }
     </div>
   );
+  }
+}
+
+
+function ShowBooks(){
+  var savedBooks = JSON.parse(localStorage.getItem('books'))
+  if(savedBooks){
+    return(
+      <div className="App">
+        <div className="container mt-10">
+          <div className="row">
+          {
+            Array.from(savedBooks).map((book,index)=>(
+              <Book key={index} title={book.title} author={book.author} tag={book.tag} rating={book.rating}/>
+            ))
+          }
+          </div>
+          </div>
+      </div>
+    )
+  }
+  else{
+    return(
+      <div>
+        <p>You haven't added any books yet!</p>
+      </div>
+    )
   }
 }
 
