227a0811935b314d8f952c8ea65ef076d0505084
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-02-25 00:42:38 -0300

    feat: Show book component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Book from './components/Book'
 
-function App() {
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    
   return (
     <div className="App">
-      Book Time    
+      <Book title="fantasy world" author="me" tag="read" rating="1 star"/>
     </div>
   );
+  }
 }
 
 export default App;
